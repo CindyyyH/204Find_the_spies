08aa66c232c6319cfc6fe8178c35364b775e91bb
lizehhaoo <149284116+lizehhaoo@users.noreply.github.com>
2023-10-29 02:20:10 -0400

    Update run.py

--- run.py
+++ run.py
@@ -9,31 +9,46 @@ config.sat_backend = "kissat"
 # Encoding that will store all of your constraints
 E = Encoding()
 
-# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
-@proposition(E)
-class BasicPropositions:
+players = ["Alice", "Bob", "Chris", "David", "Eric"]
 
-    def __init__(self, data):
-        self.data = data
+# Creating propositions that represent the state of a task for a particular player in a particular round
+#K_i: True if task round ‘i’ succeeds, False if task round ‘i’ fails；
+@proposition(E)
+class TaskSuccess:
+    def __init__(self, round_num):
+        self.round_num = round_num
+    
+    def __repr__(self):
+        return f"Ki{self.round_num}"
+
+#M_ij: In task ‘i’, True if player ‘j’ votes to accept, False if player ‘j’ votes to reject.
+@proposition(E)
+class PlayerVote:
+    def __init__(self, round_num, player_name):
+        self.round_num = round_num
+        self.player_name = player_name
+    
+    def __repr__(self):
+        return f"M{self.round_num}{self.player_name}"
+
+#R_ij: player ‘j’ attend task round ‘i’
+@proposition(E)
+class PlayerAttendance:
+    def __init__(self, round_num, player_name):
+        self.round_num = round_num
+        self.player_name = player_name
+    
+    def __repr__(self):
+        return f"R{self.round_num}{self.player_name}"
+
+#G_j: True if player ‘j’ is good person, False if not 
+@proposition(E)
+class PlayerGoodness:
+    def __init__(self, player_name):
+        self.player_name = player_name
 
     def __repr__(self):
-        return f"A.{self.data}"
-
-
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
-@proposition(E)
-class FancyPropositions:
-
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):
-        return f"A.{self.data}"
+        return f"G{self.player_name}"
 
 # Call your variables whatever you want
 a = BasicPropositions("a")
@@ -46,24 +61,64 @@ x = FancyPropositions("x")
 y = FancyPropositions("y")
 z = FancyPropositions("z")
 
-
 # Build an example full theory for your setting and return it.
 #
 #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
-def example_theory():
-    # Add custom constraints by creating formulas with the variables you created. 
-    E.add_constraint((a | b) & ~x)
-    # Implication
-    E.add_constraint(y >> z)
-    # Negate a formula
-    E.add_constraint(~(x & y))
-    # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-    # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
 
-    return E
+@constraint.at_least_one(E)
+def attendance_constraint(round_num):
+    """Constraint to ensure correct player attendance based on round number."""
+    # Odd rounds: 3 players
+    if round_num % 2 == 1:
+        players_combinations = list(combinations(players, 3))
+    # Even rounds: 2 players
+    else:
+        players_combinations = list(combinations(players, 2))
+    
+    combined_constraints = []
+    for combo in players_combinations:
+        current_constraint = PlayerAttendance(round_num, combo[0])
+        for player in combo[1:]:
+            current_constraint &= PlayerAttendance(round_num, player)
+        combined_constraints.append(current_constraint)
+    
+    return combined_constraints
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
