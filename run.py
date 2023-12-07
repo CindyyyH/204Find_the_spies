3c6486d5219ce38d6233cd0be5a9dce82d898434
CindyyyH <149280705+CindyyyH@users.noreply.github.com>
2023-12-07 15:48:06 -0500

    Update run.py

--- run.py
+++ run.py
@@ -68,14 +68,17 @@ class Suspicion:
     def __repr__(self):
         return f"S{self.player_name}"
     
-#Dj: player ‘j’ has been identified as a spy.
+#Vij:After round i, player j is voted on.
 @proposition(E)
-class Spy:
-    def __init__(self, player_name):
+class Vote:
+    def __init__(self, round_num, player_name):
+        self.round_num = round_num
         self.player_name = player_name
-
+    
     def __repr__(self):
-        return f"D{self.player_name}"
+        return f"V{self.round_num}{self.player_name}"
+
+
 
 
 
@@ -164,22 +167,22 @@ def example_theory():
 
         for j in player_names:
             # Good people can only vote for acceptance.
-            E.add_constraint(Gj(j) >> Mij(j, i))
+            E.add_constraint(G(j) >> M(j, i))
             # Spies can't vote in favor for two consecutive rounds
-            E.add_constraint(~Gj(j) >> ~(Mij(j, i) & Mij(j, i - 1)))
+            E.add_constraint(~G(j) >> ~(M(j, i) & M(j, i - 1)))
             # Spies can vote to accept or reject.
-            E.add_constraint((~Gj(j) >> Mij(j, i)) | (~Gj(j) >> ~Mij(j, i)))
+            E.add_constraint((~G(j) >> M(j, i)) | (~G(j) >> ~M(j, i)))
             # No one can participate in more than two tasks in a row
-            E.add_constraint(~(Rij(j, i) & Rij(j, i - 1) & Rij(j, i - 2)))
+            E.add_constraint(~(R(j, i) & R(j, i - 1) & R(j, i - 2)))
             # If the task the player is on fails, they will be suspected
-            E.add_constraint((Rij(j, i) >> ~Ki(i)) >> Sj(j))
+            E.add_constraint((R(j, i) >> ~K(i)) >> S(j))
            
             # At the end of rounds 6 and 12, the voted member cannot participate in the next 6 rounds of the game
             if i == 6 or i == 12:
-                E.add_constraint(~(Rij(j, i + 1) & Rij(j, i + 2) & Rij(j, i + 3) & Rij(j, i + 4) & Rij(j, i + 5) & Rij(j, i + 6)))
+                E.add_constraint(~(R(j, i + 1) & R(j, i + 2) & R(j, i + 3) & R(j, i + 4) & R(j, i + 5) & R(j, i + 6)))
         # If the number of suspects is the same, one person will be randomly selected to vote.
         if i == 18:
-            E.add_constraint(~(Gj(j) & Gj(j + 1)) >> (Vij(j, i) & Vij(j + 1, i)) >> F)
+            E.add_constraint(~(G(j) & G(j + 1)) >> (V(j, i) & V(j + 1, i)) >> F)
 
 if __name__ == "__main__":
 
